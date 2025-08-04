5cb3d10c244565603fbee5b1a44b482cbc54eef7
StickyCoolDev <hujifa.info@gmaill.com>
2025-08-04 20:23:59 +0600

    change std::endl to \n

--- Engine/src/log.cpp
+++ Engine/src/log.cpp
@@ -4,15 +4,15 @@
 namespace TextScript {
     namespace Logger {
         void log_info(std::string message){
-          std::cout << "\x1b[44m INFO  \x1b[0m "<< message << std::endl;
+          std::cout << "\x1b[44m INFO  \x1b[0m "<< message << '\n';
         }
 
         void log_warn(std::string message){
-          std::cout << "\033[48;5;208m WARN  \033[0m " << message << std::endl;
+          std::cout << "\033[48;5;208m WARN  \033[0m " << message << '\n';
         }
 
         void log_error(std::string message){
-          std::cout << "\033[101m ERROR \033[0m " << message << std::endl;
+          std::cout << "\033[101m ERROR \033[0m " << message << '\n';
         }
     }
 }
