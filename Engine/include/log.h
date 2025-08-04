649e5b46b3c7bbb96d1c3e14d6efdef13445b762
StickyCoolDev <hujifa.info@gmaill.com>
2025-08-04 11:53:29 +0600

    Update logging namespace

--- Engine/include/log.h
+++ Engine/include/log.h
@@ -10,12 +10,13 @@
   #define TEXTSCRIPT_API
 #endif
 
-namespace Logger {
-    TEXTSCRIPT_API void log_info(std::string message);
-    TEXTSCRIPT_API void log_warn(std::string message);
-    TEXTSCRIPT_API void log_error(std::string message);
+namespace TextScript {
+    namespace Logger {
+        TEXTSCRIPT_API void log_info(std::string message);
+        TEXTSCRIPT_API void log_warn(std::string message);
+        TEXTSCRIPT_API void log_error(std::string message);
+    }
 }
-
-#define LOG_INFO(MESSSAGE)  Logger::log_info(MESSSAGE)
-#define LOG_WARN(MESSSAGE)  Logger::log_warn(MESSSAGE)
-#define LOG_ERROR(MESSSAGE) Logger::log_error(MESSSAGE)
+#define LOG_INFO(MESSSAGE)  TextScript::Logger::log_info(MESSSAGE)
+#define LOG_WARN(MESSSAGE)  TextScript::Logger::log_warn(MESSSAGE)
+#define LOG_ERROR(MESSSAGE) TextScript::Logger::log_error(MESSSAGE)
--- Engine/src/log.cpp
+++ Engine/src/log.cpp
@@ -1,15 +1,18 @@
 #include "log.h"
 #include "pch.h"
-namespace Logger {
-    void log_info(std::string message){
-      std::cout << "\x1b[44m INFO  \x1b[0m "<< message << std::endl;
-    }
 
-    void log_warn(std::string message){
-      std::cout << "\033[48;5;208m WARN  \033[0m " << message << std::endl;
-    }
+namespace TextScript {
+    namespace Logger {
+        void log_info(std::string message){
+          std::cout << "\x1b[44m INFO  \x1b[0m "<< message << std::endl;
+        }
 
-    void log_error(std::string message){
-      std::cout << "\033[101m ERROR \033[0m " << message << std::endl;
+        void log_warn(std::string message){
+          std::cout << "\033[48;5;208m WARN  \033[0m " << message << std::endl;
+        }
+
+        void log_error(std::string message){
+          std::cout << "\033[101m ERROR \033[0m " << message << std::endl;
+        }
     }
 }
