c36787864e05f84c289847e6c459c5bf74e46054
StickyCoolDev <hujifa.info@gmaill.com>
2025-09-02 23:15:19 +0600

    Create Base for flask-editor

--- Editor/app.py
+++ Editor/app.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from editor!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return ""
+
+def main():
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
